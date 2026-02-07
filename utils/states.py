4bc8c3b69f77b7a631914e912f08ab5d606a00da
blackbot00 <109160013+blackbot00@users.noreply.github.com>
2026-02-07 20:43:17 +0530

    Update states.py

--- utils/states.py
+++ utils/states.py
@@ -9,4 +9,8 @@ class ChatState(StatesGroup):
     searching = State()
     on_human_chat = State()
     on_ai_chat = State()
-  
+
+# --- Idhu thaan important for Payment Screenshot fix ---
+class PremiumState(StatesGroup):
+    waiting_for_screenshot = State()
+    
